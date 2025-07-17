9e18047794f9ea223597d90f8831dff14a50d993
Eduardorg27 <eduardoromanog027@gmail.com>
2025-07-17 00:17:16 -0600

    Agregue la funcion de restar de n a 1

--- ejercicio1.js
+++ ejercicio1.js
@@ -6,4 +6,14 @@ let resultado=0
     }
     console.log(resultado)
 }
+
+function resta(numero2){
+    resultado = 0
+    for(let i=0; i<=numero2;i++){
+        resultado-=i
+
+    }
+    console.log(resultado)
+}
 suma(5)
+resta(6)
